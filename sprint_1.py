27d0e81ece30f21aafe60d84267d8b60824eb65f
sai <saikirankondapalli03@gmail.com>
2019-09-28 01:33:39 -0400

    renamed files and added US04 and US08 to master sprint_1.py

--- sprint_1.py
+++ sprint_1.py
@@ -1,10 +1,26 @@
-from userStory01 import user_story01_main
+from userStory04 import user_story04
+from userStory08 import user_story08
 from print_main import printTables
 
 def sprint_1_user_stories():
-    printTables("mytestfile.ged")
+    master_file_name="master-sprint-1-test-data.ged"
+    printTables(master_file_name)
     error_list = []
-    error_list.extend(user_story01_main()) 
+    '''
+    error_list.extend(user_story01(master_file_name)) 
+    error_list.extend(user_story02(master_file_name)) 
+    error_list.extend(user_story03(master_file_name)) 
+    '''
+    error_list.extend(user_story04(master_file_name)) 
+    '''
+    error_list.extend(user_story05(master_file_name)) 
+    error_list.extend(user_story06(master_file_name)) 
+    error_list.extend(user_story07(master_file_name)) 
+    '''
+    error_list.extend(user_story08(master_file_name)) 
+    
+    for each_error in error_list:
+        print(each_error)
 
     
 
