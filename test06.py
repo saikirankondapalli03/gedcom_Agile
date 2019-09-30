28b009b03a2ec6dc2354a7a3fe3c8acc06c4280f
jinalbangur16 <47166007+jinalbangur16@users.noreply.github.com>
2019-09-30 18:55:11 -0400

    Update test06.py

--- test06.py
+++ test06.py
@@ -8,9 +8,7 @@ class UserStory06Test(unittest.TestCase):
        error_list = us06_divo_b4_death("us06testdata.ged")
        print(error_list)
        self.assertEqual(error_list,['ERROR: FAMILY: US06: F23 : Divorce 1999-02-14 is after death 1998-12-31'])
-
-        
-        
+      
 if __name__ == '__main__':
     # note: there is no main(). Only test cases here
     unittest.main(exit=False, verbosity=2)     
