2fd5160c3239dce3a118aabe4f9cd2b0b6c85b8d
jinalbangur <44354264+jinalbangur@users.noreply.github.com>
2019-09-29 21:12:13 -0400

    organized sprint 1 functions to execute one by one

--- sprint_1.py
+++ sprint_1.py
@@ -1,52 +1,40 @@
-from userStory04 import user_story04
-from userStory08 import user_story08
+from userStory04 import us04_marr_b4_divo
+from userStory08 import us08_birth_b4_marr_parents
 from userStory03 import us03_birth_b4_death
 from userStory06 import us06_divo_b4_death
+from userStory02 import us02_birth_b4_marr
+from userStory07 import us07_less_than_150years
 from userStory01 import us01_dates_b4_curr_date
 from userStory05 import us05_marr_b4_death
-
 from print_main import printTables
 
 def sprint_1_user_stories():
-    master_file_name="finalfamtree.ged"
+    master_file_name="sprint01-testdata.ged"
     indTable,famTable= printTables(master_file_name)
     error_list = []
-    error_list.extend(us01_dates_b4_curr_date(master_file_name))
-    '''
-    error_list.extend(user_story02(master_file_name)) 
-    error_list.extend(user_story03(master_file_name)) 
-    '''
-    
+    error_list.extend(us01_dates_b4_curr_date(master_file_name)) 
+    error_list.extend(us02_birth_b4_marr(master_file_name)) 
     error_list.extend(us03_birth_b4_death(master_file_name))
-    error_list.extend(user_story04(master_file_name))
-    error_list.extend(us05_marr_b4_death(master_file_name))
-    '''
-    error_list.extend(user_story06(master_file_name)) 
-    error_list.extend(user_story07(master_file_name)) 
-    '''
-    
+    error_list.extend(us04_marr_b4_divo(master_file_name)) 
+    error_list.extend(us05_marr_b4_death(master_file_name))  
     error_list.extend(us06_divo_b4_death(master_file_name))
-    error_list.extend(user_story08(master_file_name)) 
+    error_list.extend(us07_less_than_150years(master_file_name))
+    error_list.extend(us08_birth_b4_marr_parents(master_file_name)) 
     
-    
-    
+
     for each_error in error_list:
         print(each_error)
     
     with open('sprint1output.txt','w') as file:
-        file.write('\n\nIndividuals Information----------------------->\n')
+		file.write('\n\nIndividuals Information----------------------->\n')
         file.write(indTable.get_string())
-        file.write("\n")
-        file.write('\n\nFamily Information----------------------->\n')
+		file.write("\n")
+		file.write('\n\nFamily Information----------------------->\n')
         file.write(famTable.get_string())
         file.write("\n")
         for each_error in error_list:
             file.write(each_error+"\n")
         
         
-        
-
-    
-
 if __name__ == "__main__":
     sprint_1_user_stories()
