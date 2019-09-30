befbf315c0fa4a6cc39c3f2c137faf2bb2644e2f
jinalbangur16 <47166007+jinalbangur16@users.noreply.github.com>
2019-09-30 18:39:54 -0400

    Update test01.py

--- test01.py
+++ test01.py
@@ -11,9 +11,7 @@ class UserStory01Test(unittest.TestCase):
         'ERROR: INDIVIDUAL: US01: I01: Death Date 2021-12-31 is after todays date', 
         'ERROR: FAMILY: US01: F23: Marriage Date 2020-09-29  is after todays date', 
         'ERROR: FAMILY: US01: F23: Divorce Date 2021-12-14  is after todays date'])
-
-        
-        
+    
 if __name__ == '__main__':
     # note: there is no main(). Only test cases here
     unittest.main(exit=False, verbosity=2)     
