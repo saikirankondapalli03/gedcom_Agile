7445d4861323de120062a89acaa06a7fc50f24ea
sai <saikirankondapalli03@gmail.com>
2019-10-04 13:02:22 -0400

    renamed function names to ensure test cases execution

--- test08.py
+++ test08.py
@@ -1,10 +1,10 @@
-from userStory08 import user_story08
+from userStory08 import us08_birth_b4_marr_parents
 import unittest 
 
 class UserStory08Test(unittest.TestCase):
 
     def test_us_08(self):
-       error_list = user_story08("us08testdata.ged")
+       error_list = us08_birth_b4_marr_parents("us08testdata.ged")
        print(error_list)
        self.assertEqual(error_list,['ANOMALY: FAMILY: US08: I19: born 1981-02-13 before marriage on 1981-02-14',
        'ANOMALY: FAMILY: US08: I26: born 1984-12-14 after divorce on 1981-02-14'])
