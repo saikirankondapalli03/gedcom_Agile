b243e94565e7345ae005300a9452a935fcc5aa76
jinalbangur16 <47166007+jinalbangur16@users.noreply.github.com>
2019-10-13 22:05:28 -0400

    Update sprint2_alltests.py

--- sprint2_alltests.py
+++ sprint2_alltests.py
@@ -1,11 +1,22 @@
 import unittest
-from us_test.sprint02_tests.test12 import UserStory12Test
-from us_test.sprint02_tests.test13 import UserStory13Test
+from us_test.sprint02_tests.test09 import UserStory12Test
+from us_test.sprint02_tests.test10 import UserStory13Test
+from us_test.sprint02_tests.test11 import UserStory12Test
+from us_test.sprint02_tests.test12 import UserStory13Test
+from us_test.sprint02_tests.test13 import UserStory12Test
+from us_test.sprint02_tests.test14 import UserStory13Test
+from us_test.sprint02_tests.test15 import UserStory12Test
+from us_test.sprint02_tests.test16 import UserStory13Test
 
 class Sprint02_Test(unittest.TestCase):
+    UserStory09Test()
+    UserStory10Test()
+    UserStory11Test()
     UserStory12Test()
     UserStory13Test()
-        
-        
+    UserStory14Test()
+    UserStory15Test()
+    UserStory16Test()
+
 if __name__ == "__main__":
     unittest.main()
