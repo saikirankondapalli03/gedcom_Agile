9c737ac72cc2a4289273ee8c8b2a0f626436a594
Sai Kiran Kondapalli <saikirankondapalli03@gmail.com>
2019-10-15 00:19:31 -0400

    test-travis-print

--- ParserModule.py
+++ ParserModule.py
@@ -7,6 +7,7 @@ from famClass import famClass
 from indClass import indClass
 
 def dateFormating(myDate):
+    print("Sai")
     myMonths = {'JAN': '01', 'FEB': '02', 'MAR': '03', 'APR': '04', 'MAY': '05', 'JUN': '06', 'JUL': '07', 'AUG': '08', 'SEP': '09', 'OCT': '10', 'NOV': '11', 'DEC': '12'}
     myDate = myDate.split()
     if int(myDate[0]) in range(1,10):
